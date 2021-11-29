02499c161b18b7cd798cdb13f51191bb43c689c8
Valéria Barci <vahc.201@gmail.com>
2021-11-29 23:10:53 +0000

    Atualizado o campo value cpf do admin

--- Back/src/api/validacoes/validacaoadmin.js
+++ Back/src/api/validacoes/validacaoadmin.js
@@ -1,10 +1,10 @@
 var tentativas = 3; // Variável com número de tentativas
 // A função executa após o click no botão login
 
-function validarUsuario(){
-    var usuario = document.getElementById("usuario").value;
+function validarAdmin(){
+    var cpf = document.getElementById("cpf").value;
     var senha = document.getElementById("senha").value;
-        if ( usuario == "admin" && senha == "admin#123"){
+        if ( cpf == "21824961065" && senha == "admin#123"){
         alert ("Login realizado com sucesso");
         window.location = "dashboard.html"; // Redirecionamento para página do dashboard
         return false;
@@ -14,7 +14,7 @@ function validarUsuario(){
         alert("Você realizou "+tentativas+" tentativas;");
     // Desabilitar quando atingir o número máximo de tentativas
     if( tentativas == 0){
-    document.getElementById("usuario").disabled = true;
+    document.getElementById("cpf").disabled = true;
     document.getElementById("senha").disabled = true;
     document.getElementById("submit").disabled = true;
 return false;
