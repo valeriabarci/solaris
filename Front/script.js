115a1585d989d15c561994c0bf83a10e51639ef9
Valéria Barci <73722482+valeriabarci@users.noreply.github.com>
2021-12-01 20:47:06 -0300

    Ajustes no script

--- Front/script.js
+++ Front/script.js
@@ -1,12 +1,6 @@
 function openLoginForm(){
     document.body.classList.add("showLoginForm");
-  }
+}
   function closeLoginForm(){
     document.body.classList.remove("showLoginForm");
-  }
-  function openEsqueciMeuForm(){
-    document.body.classList.add("showEsqueciMeuForm");
-  }
-  function closeEsqueciMeuForm(){
-    document.body.classList.remove("showEsqueciMeuForm");
-  }
+}
